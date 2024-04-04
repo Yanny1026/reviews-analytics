82ada4badb4b770fbc50f756bbcee706de7e7fc6
Yanny <SUM_1026@yahoo.com.hk>
2024-04-04 18:57:30 +0800

    add filter funcion

--- read.py
+++ read.py
@@ -12,3 +12,15 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度為', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+g = []
+for d in data:
+	if 'good' in d:
+		g.append(d)
+print('一共有', len(g), '筆留言有good的字眼')
